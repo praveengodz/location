77d2b0a8759a392946ea1486a71337c986527b03
APPLE <apple@praveen.local>
2013-06-18 15:15:45 +0530

    Initial Commit

---
+++ location/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  location
+//
+//  Created by APPLE on 18/06/13.
+//  Copyright (c) 2013 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ location/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  location
+//
+//  Created by APPLE on 18/06/13.
+//  Copyright (c) 2013 APPLE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
